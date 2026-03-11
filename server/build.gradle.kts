8215e40b9a84a160a41b3d02cef04dcb14b50b12
Robert Eric Reeves <robertericreeves@gmail.com>
2026-03-11 14:57:20 -0500

    Fix command-executor version to 1.7.0 (unchanged in 1.7.1)

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("com.datadatdat:remote-sdk:1.7.1")
-    implementation("com.datadatdat:command-executor:1.7.1")
+    implementation("com.datadatdat:command-executor:1.7.0")
     implementation("com.google.code.gson:gson:2.13.2")
     implementation("org.slf4j:slf4j-api:2.0.17")
     implementation(project(path = ":engine", configuration = "default"))
