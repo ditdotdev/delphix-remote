a453afd707236553b015c03d5fd50a36556e3d79
Robert Eric Reeves <robertericreeves@gmail.com>
2026-04-14 13:32:34 -0500

    Update dependencies to 1.8.5

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.datadatdat:remote-sdk:1.8.4")
+    implementation("com.datadatdat:remote-sdk:1.8.5")
     implementation("com.datadatdat:command-executor:1.8.5")
     implementation("com.google.code.gson:gson:2.13.2")
     implementation("org.slf4j:slf4j-api:2.0.17")
